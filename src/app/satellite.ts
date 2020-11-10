50aa573d05e59dd5a0b2fbda8d93c6ebafc0b58e
pdunlap <xtrishayy@yahoo.com>
2020-11-09 20:12:08 -0600

    Finished

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -4,18 +4,20 @@ type: string;
 operational: boolean;
 orbitType: string;
 launchDate: string;
-constructor(name:string, type: string, launchDate: string, orbitType:string, operational: boolean, shouldShowWarning: boolean) {
+// shouldShowWarning: boolean;
+
+constructor(name:string, type: string, launchDate: string, orbitType:string, operational: boolean) {
     this.name = name;
     this.type = type;
     this.operational = operational;
     this.orbitType = orbitType;
     this.launchDate = launchDate;
+    
     }
     shouldShowWarning():boolean {
-        if(this.type.toLowerCase() == 'space debris') {
-        return  true;
-    } else {
-        return false;
-    }
+        return (this.type.toLowerCase() === 'space debris') 
+        
+    
+    
 }
 }
